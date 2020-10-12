6a80c74e994b12e8fd5d4317615e560a1a8cef63
Cora Krynick <krynickcora@gmail.com>
2020-10-12 14:51:37 -0500

    got the api moved into fetchShow

---
+++ src/api/fetchShow.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+export const fetchShow = () => {
+    return axios
+    .get(
+        "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+    )
+    .then((res) => {
+        console.log(res);
+        return res;
+    })
+    .catch(err => {
+        console.log("Error: ", err.message);
+        return err;
+    })
+    
+}
+
+// useEffect(() => {
+//     const fetchShow = () => {
+//       axios
+//         .get(
+//           "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+//         )
+//         .then(res => {
+//           setShow(res.data);
+//           setSeasons(formatSeasons(res.data._embedded.episodes));
+//         });
+//     };
+//     fetchShow();
+//   }, []);
